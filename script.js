95754a75596d9b10c20b1f2b33624ff63baed624
synoratska <m.synoratska@gmail.com>
2022-05-24 00:29:55 +0300

    mobile menu added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btn = document.querySelector('#menu-btn');
-
-
+const nav = document.querySelector('#menu');
 
 function navToggle() {
     btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
 }
 
 btn.addEventListener('click', navToggle);

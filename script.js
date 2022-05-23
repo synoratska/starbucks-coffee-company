e0a142dbd10b5c354cca57bf4e1efe33d484c367
synoratska <m.synoratska@gmail.com>
2022-05-24 00:30:28 +0300

    final changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const btn = document.querySelector('#menu-btn');
-const nav = document.querySelector('#menu');
+const btn = document.querySelector("#menu-btn");
+const nav = document.querySelector("#menu");
 
 function navToggle() {
-    btn.classList.toggle('open')
-    nav.classList.toggle('hidden')
-    document.body.classList.toggle('no-scroll')
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no-scroll");
 }
 
-btn.addEventListener('click', navToggle);
+btn.addEventListener("click", navToggle);
